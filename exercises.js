c2451956697e8a1b3bb9afdcfcfb894661c1284c
returnlove <iamsainath2@gmail.com>
2018-01-15 22:34:25 +0530

    some exercises

---
+++ exercises.js
@@ -0,0 +1,73 @@
+//  Write a JavaScript program to list the properties of a JavaScript object.
+
+var student = { 
+name : "David Rayy", 
+sclass : "VI", 
+rollno : 12 };
+
+for(var key in student)
+{	
+	// print prop
+	console.log(key);
+	//print val
+	console.log(student[key]);
+}
+
+// Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+
+var student = { 
+name : "David Rayy", 
+sclass : "VI", 
+rollno : 12 };
+
+console.log("before del");
+console.log(student);
+console.log("after del");
+delete(student.rollno);
+console.log(student);
+
+// Write a JavaScript program to get the length of an JavaScript object.
+
+var student = { 
+name : "David Rayy", 
+sclass : "VI", 
+rollno : 12,
+inner_obj: {'k1': 's'}
+ }; 
+
+var i = 0;
+for(var key in student)
+{
+	i += 1;
+}
+console.log(i);
+
+
+//Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books
+
+
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+   
+   
+for(var i= 0; i < library.length; i++)
+{
+	console.log(library[i]["author"]);
+	console.log(library[i]["title"]);
+	console.log(library[i]["readingStatus"]);
+	console.log("####");
+}
