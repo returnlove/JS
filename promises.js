79984accc1dc9c0936e54059daf85666a8279789
Sainath <iamsainath2@gmail.com>
2018-01-22 00:02:56 +0530

    promises in js

---
+++ promises.js
@@ -0,0 +1,12 @@
+//https://www.youtube.com/watch?v=s6SH72uAn3Q&t=18s
+
+
+/*
+
+similar to call backs in JS but more cleaned way (check how call back works)
+
+promises can wait for some action to perform before other action gets performed.
+Basically whenever there is a dependency we can use promises to check if any action is completed
+before you want to start some action
+
+*/
