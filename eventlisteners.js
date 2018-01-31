17e7a6eb6a973a3f91021c6d4819ce58c543d97f
Sainath <iamsainath2@gmail.com>
2018-01-31 23:49:04 +0530

    event listeners in js

---
+++ eventlisteners.js
@@ -0,0 +1,12 @@
+//https://www.google.co.in/
+
+
+document.getElementsByClassName("gb_P")[1].addEventListener("click", function(){
+console.log("test");
+})
+
+
+
+function con_log(){console.log("con log function")}
+
+document.getElementsByClassName("gb_P")[1].addEventListener("click", con_log)
